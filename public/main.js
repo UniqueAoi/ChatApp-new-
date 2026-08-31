ae83bf269a50816323ac0254b962f6fa00296f47
Phoo <pthtftzaw@gmail.com>
2026-08-31 13:41:10 +0630

    Send message

--- public/main.js
+++ public/main.js
@@ -17,15 +17,35 @@ socket.on('clients-total', (data) => {
 })
 
 function sendMessage() {
-    console.log(messageInput.value)
-    const data ={
+    if(messageInput.value === '') return
+    //console.log(messageInput.value)
+    const data = {
         name: nameInput.value,
         message: messageInput.value,
         dateTime: new Date()
     }
     socket.emit('message', data)
+    addMessageToUI(true, data)
+    messageInput.value =''
 }
 
 socket.on('chat-message', (data) => {
-    console.log(data)
+    //console.log(data)
+    addMessageToUI(false, data)
 })
+
+function addMessageToUI(isOwnMessage, data) {
+    const element = `
+        <li class="${isOwnMessage ? "message-right" : "message-left"}">
+                <p class="message">
+                    ${data.message}
+                    <span>${data.name} • ${moment(data.dateTime).fromNow()}</span>
+                </p>
+        </li>`
+    messageContainer.innerHTML += element  
+    scrollToBottom()      
+}
+
+function scrollToBottom(){
+    messageContainer.scrollTo(0, messageContainer.scrollHeight)
+}
